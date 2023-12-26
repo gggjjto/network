122c769533f5242fd9adf1171a708880ee6c41e8
qcloud <ubuntu@localhost.localdomain>
2023-12-26 11:30:04 +0800

    更新注释

--- internetChat/client.cpp
+++ internetChat/client.cpp
@@ -21,59 +21,62 @@ int main(int argc, char* argv[])
         cout<<"usage:"<<basename(argv[0])<<"ip_address port_number\n";
         return 1;
     }
-    const char* ip = argv[1];
-    int port = atoi(argv[2]);
+    const char* ip = argv[1]; // ip地址
+    int port = atoi(argv[2]); // 字符串转port
 
-    struct sockaddr_in server_address;
+    // 创建套接字，
+    struct sockaddr_in server_address; // 表示IPv4地址和端口信息
     bzero(&server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
     inet_pton(AF_INET, ip, &server_address.sin_addr);
     server_address.sin_port = htons(port);
 
-    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-    assert(sockfd >= 0);
-    if(connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0){
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0); // 创建socket
+    assert(sockfd >= 0); // 创建失败为 -1
+    if(connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) // 和server_address建立网络通信连接
+    {
         cout<<"connection failed\n";
         close(sockfd);
         return 1;
     }
     pollfd fds[2];
     fds[0].fd = 0;
-    fds[0].events =POLLIN;
+    fds[0].events =POLLIN; // 等待事件
     fds[0].revents = 0;
     fds[1].fd = sockfd;
-    fds[1].revents = POLLIN |POLLRDHUP;
+    fds[1].events = POLLIN |POLLRDHUP; // 可读|挂起
     fds[1].revents = 0;
 
     char read_buf[BUFFER_SIZE];
     int pipefd[2];
-    int ret = pipe(pipefd);
+    int ret = pipe(pipefd); // 一对文件描述符，一个读取，一个写入
     assert(ret != -1);
 
     while(1)
     {
-        ret = poll(fds, 2, -1);
+        ret = poll(fds, 2, -1); // -1 等待
         if(ret < 0)
         {
             cout<<"poll failure\n";
             break;
         }
 
-        if(fds[1].revents & POLLRDHUP)
+        if(fds[1].revents & POLLRDHUP) // 通信结束
         {
             cout<<"server close the connection\n";
             break;
         }
-        else if(fds[1].revents & POLLIN)
+        else if(fds[1].revents & POLLIN) // 通信开始
         {
             memset(read_buf,'\0',BUFFER_SIZE);
-            recv(fds[1].fd, read_buf, BUFFER_SIZE - 1, 0);
+            recv(fds[1].fd, read_buf, BUFFER_SIZE - 1, 0); // 接受数据
             cout<<read_buf;
         }
-        if(fds[0].revents & POLLIN)
+        if(fds[0].revents & POLLIN) // 上传数据
         {
             ret = splice(0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
             ret = splice(pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+            printf("pipefd[0]:%d pipefd[0]:%d\n",pipefd[0],pipefd[1]);
         }
     }
     close(sockfd);
--- internetChat/server.cpp
+++ internetChat/server.cpp
@@ -24,7 +24,9 @@ struct client_data
     char* write_buf;
     char buf[BUFFER_SIZE];
 };
-
+/**
+ * 设置为非阻塞模式 
+*/
 int setnonblocking(int fd)
 {
     int old_option = fcntl(fd, F_GETFL);
@@ -44,6 +46,7 @@ int main(int argc, char* argv[])
     const char* ip = argv[1];
     int port = atoi(argv[2]);
 
+    // 创建套接字，监听连接
     int ret = 0;
     struct sockaddr_in address;
     bzero(&address, sizeof(address));
@@ -54,11 +57,11 @@ int main(int argc, char* argv[])
     int listenfd = socket(PF_INET, SOCK_STREAM, 0);
     assert(ret != -1);
 
-    ret = listen(listenfd, 5);
+    ret = listen(listenfd, 5); // 监听服务器端开始传入连接请求的函数，等待连接数5
 
     assert(ret != 1);
-    client_data* users = new client_data[FD_LIMIT];
-    pollfd fds[USER_LIMIT + 1];
+    client_data* users = new client_data[FD_LIMIT]; // 储存客户端数据
+    pollfd fds[USER_LIMIT + 1]; // 监视文件描述符上发生事件的数据结构
     int user_counter = 0;
     for(int i = 1; i <= USER_LIMIT; i++)
     {
@@ -79,28 +82,30 @@ int main(int argc, char* argv[])
         }
 
         for(int i = 0; i< user_counter + 1; i++){
+            // 该文件描述符开始传入
             if((fds[i].fd == listenfd) && (fds[i].revents & POLLIN))
             {
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength = sizeof(client_address);
-                int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength);
+                int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength); // 接受客户端的连接请求，并存储客户端的地址信息
                 if(connfd < 0){
                     printf("errno is: %d\n", errno);
                     continue;
                 }
+                printf("地址：%d\n",connfd);
                 if(user_counter >= USER_LIMIT)
                 {
                     const char* info = "too many users\n";
-                    cout<<info;
-                    send(connfd, info, strlen(info), 0);
+                    cout<<info<<endl;
+                    send(connfd, info, strlen(info), 0); // 用于发送数据到已连接的对等方
                     close(connfd);
                     continue;
                 }
-                user_counter++;
+                user_counter++; // 连接的用户数量
                 users[connfd].address = client_address;
                 setnonblocking(connfd);
-                fds[user_counter].fd = connfd;
-                fds[user_counter].events = POLLIN | POLLRDHUP | POLLERR;
+                fds[user_counter].fd = connfd; 
+                fds[user_counter].events = POLLIN | POLLRDHUP | POLLERR; //文件描述符上发生了错误事件，例如连接被关闭或出现读写错误
                 fds[user_counter].revents = 0;
                 cout<<"comes a new user, now have"<<user_counter<< "users\n";
             }
@@ -110,13 +115,14 @@ int main(int argc, char* argv[])
                 char errors[100];
                 memset(errors, '\0', 100);
                 socklen_t length = sizeof(errors);
-                if(getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0)
+                if(getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0) // 拿出错误
                 {
                     cout<<"get socket option failed\n";
                 }
+                cout<<errors<<'\n';
                 continue;
             }
-            else if(fds[i].revents & POLLRDHUP)
+            else if(fds[i].revents & POLLRDHUP) // 挂起结束
             {
                 users[fds[i].fd] = users[fds[user_counter].fd];
                 close(fds[i].fd);
@@ -125,12 +131,12 @@ int main(int argc, char* argv[])
                 user_counter--;
                 cout<<"a client left\n";
             }
-            else if(fds[i].revents & POLLIN)
+            else if(fds[i].revents & POLLIN) // 收到请求开始
             {
                 int connfd = fds[i].fd;
                 memset(users[connfd].buf, '\0', BUFFER_SIZE);
-                ret = recv(connfd, users[connfd].buf, BUFFER_SIZE - 1, 0);
-                printf("get %d bytes of client data %s from %d\n", ret, users[connfd].buf, connfd);
+                ret = recv(connfd, users[connfd].buf, BUFFER_SIZE - 1, 0); // 接受数据
+                printf("get %d bytes of client data %s from %d\n", ret, users[connfd].buf, connfd); // 说明数据的各项数据
                 if(ret < 0)
                 {
                     if(errno != EAGAIN)
@@ -151,19 +157,19 @@ int main(int argc, char* argv[])
                             continue;
                         }
 
-                        fds[j].events |= ~POLLIN;
+                        fds[j].events |= ~POLLIN; // 清除POLLIN位
                         fds[j].events |= POLLOUT;
                         users[fds[j].fd].write_buf = users[connfd].buf;
                     }
                 }
             }
-            else if(fds[i].revents & POLLOUT)
+            else if(fds[i].revents & POLLOUT) // 写
             {
                 int connfd = fds[i].fd;
                 if(!users[connfd].write_buf){
                     continue;
                 }
-                ret = send( connfd, users[connfd].write_buf,strlen(users[connfd].write_buf), 0);
+                ret = send( connfd, users[connfd].write_buf,strlen(users[connfd].write_buf), 0); // 发送数据
                 users[connfd].write_buf = NULL;
                 fds[i].events |= ~POLLOUT;
                 fds[i].events |= POLLIN;
